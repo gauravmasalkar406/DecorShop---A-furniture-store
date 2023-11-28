9090039bb4c6300c3adb3d64427c5402ac8319d5
Gaurav Masalkar <gaurav.official405@gmail.com>
2023-11-28 17:50:00 +0530

    changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    manifest: true,
-    rollupOptions: {
-      input: "/src/main.jsx",
-    },
-  },
+  plugins: [react()],
 });
