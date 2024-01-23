873c3f400ca2f8a69113946f6417751ee302e53c
Gaurav Masalkar <gaurav.official405@gmail.com>
2024-01-23 18:49:35 +0530

    seo improved

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -14,6 +14,7 @@ const Home = () => {
           name="description"
           content="Discover a diverse selection of high-quality home decor items at DecorShop. Elevate your living space with unique products and accessories."
         />
+        {/* Open Graph meta tags for better social media sharing */}
         <meta
           property="og:title"
           content="Quality Home Decor Products | DecorShop"
@@ -30,7 +31,27 @@ const Home = () => {
           property="og:url"
           content="https://starlit-boba-8d5fb7.netlify.app"
         />
+
+        {/* Twitter Card meta tags for Twitter sharing */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:title"
+          content="Quality Home Decor Products | DecorShop"
+        />
+        <meta
+          name="twitter:description"
+          content="Explore a diverse selection of high-quality home decor items at DecorShop. Elevate your living space with unique products and accessories."
+        />
+        <meta
+          name="twitter:image"
+          content="https://decorshop-jrzm.onrender.com/uploads/BlackSofaSet-WoodCraf01.webp"
+        />
+
+        {/* Additional SEO meta tags */}
+        <meta name="robots" content="index, follow" />
+        <link rel="canonical" href="https://starlit-boba-8d5fb7.netlify.app" />
       </Helmet>
+
       <Header />
       <Highlights />
       <Featured />
