1b72b743c13f4c71d4a226e591ab9ecb7a2302b6
gauravm406 <gaurav.official405@gmail.com>
2025-01-01 12:54:23 +0530

    folder names changed

---
+++ client/src/components/Rating/Stars.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { FaStar } from "react-icons/fa";
+import s from "./stars.module.css";
+
+const StarRating = ({ rating }) => {
+  const roundedRating = Math.round(rating * 2) / 2;
+
+  const stars = Array.from({ length: 5 }, (_, index) => (
+    <FaStar
+      key={`${index}${rating}`}
+      className={index < roundedRating ? s.filled : s.empty}
+    />
+  ));
+
+  return <div className={s.star_rating}>{stars}</div>;
+};
+
+export default StarRating;
